63d8bb3587d7cba6b56f671e42562b252255cc24
hcahca 🍌 <hanna@iamhanna.de>
2022-04-19 22:09:28 +0200

    setup basic app with theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,101 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import {
+  Container,
+  createTheme,
+  Grid,
+  Button,
+  Paper,
+  ThemeProvider,
+  useTheme,
+  Box,
+  Stack,
+  ButtonGroup,
+} from '@mui/material';
+import {createContext, useContext, useMemo, useState} from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const ColorModeContext = createContext({toggleColorMode: () => {}});
+
+export const ThemedApp = () => {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <Box
+      sx={{
+        backgroundColor: theme.palette.background.default,
+        minHeight: '100vh',
+        width: '100vw',
+        p: 2,
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Stack direction="row" justifyContent="end" alignItems="center">
+            <ButtonGroup variant="text" aria-label="text button group" color="secondary">
+              <Button disabled={theme.palette.mode === 'dark'} onClick={colorMode.toggleColorMode}>
+                Dark
+              </Button>
+              <Button disabled={theme.palette.mode === 'light'} onClick={colorMode.toggleColorMode}>
+                Light
+              </Button>
+            </ButtonGroup>
+          </Stack>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper elevation={1}>
+            <Box
+              sx={{
+                p: 2,
+              }}
+            >
+              Prompt
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper elevation={1}>
+            <Box
+              sx={{
+                p: 2,
+              }}
+            >
+              Result
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-export default App
+export const App = () => {
+  const [mode, setMode] = useState<'light' | 'dark'>(
+    window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'
+  );
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <ThemedApp />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
